be8c1ff19e078d82fccac2f30ac45330c6bff957
Adrián Bailador Panero <adrianbailador@hotmail.com>
2021-02-28 17:41:23 +0000

    Mostrar notas

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Note } from "./Note.js";
 export default function App(props) {
   const [notes, setNotes] = useState(props.notes);
   const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
 
   const handleChange = (event) => {
     setNewNote(event.target.value);
   };
 
-  const handleClick = (event) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
     console.log("crear nota");
     console.log(newNote);
     const noteToAddToState = {
@@ -21,8 +23,13 @@ export default function App(props) {
     };
     console.log(noteToAddToState);
 
-    setNotes(notes.concat(noteToAddToState));
+    //setNotes(notes.concat(noteToAddToState));
+    setNotes([...notes, noteToAddToState]); //Esta es una forma mucho mas comoda
     setNewNote("");
+  };
+
+  const handleShowAll = () => {
+    setShowAll(() => !showAll);
   };
 
   if (typeof notes === "undefined" || notes.length === 0) {
@@ -32,20 +39,28 @@ export default function App(props) {
   return (
     <div>
       <h1>Notes</h1>
+      <button onClick={handleShowAll}>
+        {showAll ? "Show only important" : "Show All"}
+      </button>
       <ol>
-        {notes.map((note) => (
-          <Note
-            key={note.id}
-            id={note.id}
-            content={note.content}
-            date={note.date}
-          />
-        ))}
+        {notes
+          .filter((note) => {
+            if (showAll === true) return note;
+            return note.important === true;
+          })
+          .map((note) => (
+            <Note
+              key={note.id}
+              id={note.id}
+              content={note.content}
+              date={note.date}
+            />
+          ))}
       </ol>
-      <div>
+      <form onSubmit={handleSubmit}>
         <input type="text" onChange={handleChange} value={newNote} />
-        <button onClick={handleClick}>Crear nota</button>
-      </div>
+        <button>Crear nota</button>
+      </form>
     </div>
   );
 }

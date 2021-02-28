97e733d09b9dc03a6b452c82e6796fd0940c07af
Adrián Bailador Panero <adrianbailador@hotmail.com>
2021-02-28 16:27:12 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./styles.css";
+
+const notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
+  }
+];
+
+export default function App() {
+  if (typeof notes === "undefined" || notes.length === 0) {
+    return "No tenemos notas que mostrar";
+  }
+
+  return (
+    <div>
+      {notes.map((note) => (
+        <li key={note.id}>
+          <p>
+            <strong>{note.id}</strong>{" "}
+          </p>
+          <small>
+            <time>{note.date}</time>
+          </small>
+        </li>
+      ))}
+    </div>
+  );
+}
